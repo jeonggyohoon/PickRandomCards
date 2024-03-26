b9b31cef6b3426617040cb87546a3153ad928745
jkh38 <jkh38@EMFOCUS>
2024-03-26 09:11:48 +0900

    First cardGame

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -10,6 +10,35 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Enter the number of cards to pick : ");
+                string line = Console.ReadLine();
+
+                int numberOfCards;
+
+                if (int.TryParse(line, out numberOfCards))
+                {
+                    foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                    {
+                        Console.WriteLine(card);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+                Console.Write("Do you want restart? : (R/E) ");
+                string stopLine = Console.ReadLine();
+                if (stopLine == "R")
+                {
+                    continue;
+                }
+                if (stopLine == "E")
+                {
+                    break;
+                }
+            }
         }
     }
 }
